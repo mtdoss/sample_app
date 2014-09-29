9b34a7f7dddc5f432f2368778aad96b3230a2a16
Mark Doss <mtdoss@gmail.com>
2014-09-28 22:13:41 -0700

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe User do
   before { @user = User.new(name: "Example User", email: "user@example.com", 
-  							password: "foobar", password_confirmation: "foobar") }
+   password: "foobar", password_confirmation: "foobar") }
 
   subject { @user }
 
@@ -12,7 +12,14 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:remember_token) }
   it { should be_valid }
+
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
   describe "when name is not present" do
   	before { @user.name = " " }
@@ -26,8 +33,8 @@ describe User do
   		@user.email = mixed_case_email
   		@user.save
   		expect(@user.reload.email).to eq mixed_case_email.downcase
-  		end
-  	end
+    end
+  end
 
   describe "return value of authenticate method" do
   	before { @user.save }
@@ -52,7 +59,7 @@ describe User do
   describe "when password is not present" do
   	before do
   		@user = User.new(name: "Example User", email: "user@example.com", 
-  						password: " ", password_confirmation: " ")
+        password: " ", password_confirmation: " ")
   	end
   	it {should_not be_valid }
   end
@@ -70,37 +77,36 @@ describe User do
   describe "when email format is invalid" do
   	it "should be invalid" do
   		addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com
-  						foo@bar..com]
-  		addresses.each do |invalid_address|
-  			@user.email = invalid_address
-  			expect(@user).not_to be_valid
-  		end
-  	end
+        foo@bar..com]
+        addresses.each do |invalid_address|
+         @user.email = invalid_address
+         expect(@user).not_to be_valid
+       end
+     end
+   end
+
+   describe "when email format is valid" do
+     it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do | valid_address|
+       @user.email = valid_address
+       expect(@user).to be_valid
+     end
+   end
+ end
+
+ describe "when email address is already taken" do
+   before do
+    user_with_same_email = @user.dup
+    user_with_same_email.email = @user.email.upcase
+    user_with_same_email.save
   end
 
-  describe "when email format is valid" do
-  	it "should be valid" do
-  		addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-  		addresses.each do | valid_address|
-  			@user.email = valid_address
-  			expect(@user).to be_valid
-  		end
-  	end
-  end
-
-  describe "when email address is already taken" do
-  	before do
-  		user_with_same_email = @user.dup
-  		user_with_same_email.email = @user.email.upcase
-  		user_with_same_email.save
-  	end
-
-  	it {should_not be_valid }
-  end
-
-  describe "when name is too long" do
-  	before { @user.name = "a" * 51 }
-  	it { should_not be_valid }
-  end
+  it {should_not be_valid }
 end
 
+describe "when name is too long" do
+ before { @user.name = "a" * 51 }
+ it { should_not be_valid }
+end
+end
